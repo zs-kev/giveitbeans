c2a70d27f55b4a48579b2c58e92d9524cf27642e
Kevin Simon <keviniansimon@gmail.com>
2023-08-28 14:17:10 +0200

    style: updated tailwind config

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,16 +1,29 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-darkpattern">
       <div>
-        <p>Quality coffee sourced globally</p>
+        <p className="font-Guy text-8xl text-white text-center">
+          Quality coffee sourced globally
+        </p>
       </div>
       <div>
-        <div>
+        <Image
+          src="/assets/logo/Logo-vertical-white.svg"
+          alt="Give it beans"
+          width={153}
+          height={87}
+          className="mx-auto"
+        />
+        <div className="flex items-center justify-center">
           <Link href={''}>Terms & Conditions</Link>
           <Link href={''}>Privacy Policy</Link>
-          <p>Give It Beans</p>
+          <p className="text-muted text-base">
+            &copy; Give It Beans {currentYear}
+          </p>
         </div>
       </div>
     </footer>
